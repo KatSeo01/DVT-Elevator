b5dd4594024e3755a5fac9b3b1c7334a537f5fe2
Katlego Seoketsa <33416076+KatSeo01@users.noreply.github.com>
2024-01-09 11:46:27 +0200

    Added ElevatorFactory

---
+++ ElevatorFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVT_Elevator
+{
+    //Creates a new Elevator instance
+    class ElevatorFactory : IElevatorFactory
+    {
+        public IElevator CreateElevator(int numFloors)
+        {
+            return new Elevator(numFloors);
+        }
+    }
+
+
+}
