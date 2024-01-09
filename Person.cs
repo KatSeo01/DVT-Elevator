ba308581e4fe5e36789ca5c08041968046b198fb
Katlego Seoketsa <33416076+KatSeo01@users.noreply.github.com>
2024-01-09 11:53:15 +0200

    Added the Person class

---
+++ Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVT_Elevator
+{
+    //The class tracks the person's starting and destination floors.
+    class Person : IPassenger
+    {
+        private int startFloor;
+        private int destinationFloor;
+
+        public Person(int startFloor, int destinationFloor)
+        {
+            this.startFloor = startFloor; //The person's starting floor
+            this.destinationFloor = destinationFloor; //The person's destination floor
+        }
+
+        public int CurrentFloor => this.startFloor;
+
+        public int DestinationFloor => this.destinationFloor;
+
+        // Checks if the person has reached their destination.
+        public bool HasReachedDestination()
+        {
+            return this.startFloor == this.destinationFloor;
+        }
+    }
+}
