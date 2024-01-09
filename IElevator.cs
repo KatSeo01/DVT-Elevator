7101b0b69266d5e862abeabc9e92cb7a56850ae7
Katlego Seoketsa <33416076+KatSeo01@users.noreply.github.com>
2024-01-09 11:47:38 +0200

    Added IElevator class

---
+++ IElevator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVT_Elevator
+{
+    //The IElevator interface defines methods: CurrentFloor, and AddPerson
+    interface IElevator
+    {
+        int CurrentFloor { get; }
+        void AddPerson(Person person);
+    }
+}
