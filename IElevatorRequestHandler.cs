8c0b874dbb6d134ed159ed77dfc426e753602dd9
Katlego Seoketsa <33416076+KatSeo01@users.noreply.github.com>
2024-01-09 11:50:19 +0200

    Added the IElevatorRequestHandler

---
+++ IElevatorRequestHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVT_Elevator
+{
+    interface IElevatorRequestHandler
+    {
+        Task RequestElevatorAsync(int fromFloor, int toFloor, int numPassengers);
+    }
+}
