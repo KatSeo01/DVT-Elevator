50f86049f11b05ca967b38827a9c3492dd0b04aa
Katlego Seoketsa <33416076+KatSeo01@users.noreply.github.com>
2024-01-09 11:48:48 +0200

    Added the ElevatorFactory Interface

---
+++ IElevatorFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVT_Elevator
+{
+    interface IElevatorFactory
+    {
+        IElevator CreateElevator(int numFloors);
+    }
+}
