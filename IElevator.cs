ca004eb163af372de1f4ed245e77e615370636d1
Katlego Seoketsa <33416076+KatSeo01@users.noreply.github.com>
2024-01-09 12:11:31 +0200

    Modified the Elevator and IElevator classes

--- IElevator.cs
+++ IElevator.cs
@@ -7,9 +7,28 @@ using System.Threading.Tasks;
 namespace DVT_Elevator
 {
     //The IElevator interface defines methods: CurrentFloor, and AddPerson
-    interface IElevator
+    interface IElevator : IElevatorMovement
     {
         int CurrentFloor { get; }
+        Direction CurrentDirection { get; }
+        ElevatorState State { get; }
+        List<Person> Passengers { get; }
+        int PassengerLimit { get; set; }
         void AddPerson(Person person);
+        void Update();
     }
+
+    public enum Direction
+    {
+        Up,
+        Down,
+        None
+    }
+
+    public enum ElevatorState
+    {
+        Moving,
+        Stationary
+    }
+
 }
