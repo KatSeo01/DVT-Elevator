957d24c2d97ea83be1e7b713612d6cb014f42f30
Katlego Seoketsa <33416076+KatSeo01@users.noreply.github.com>
2024-01-09 11:51:49 +0200

    Added the IPassenger

---
+++ IPassenger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVT_Elevator
+{
+    interface IPassenger
+    {
+        int CurrentFloor { get; }
+        int DestinationFloor { get; }
+        bool HasReachedDestination();
+    }
+
+}
